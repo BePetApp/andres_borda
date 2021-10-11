fd4610c838353fd6cf4f2c694cb1e7d5e1a8d77e
Andres-Borda <ajborda4@misena.edu.co>
2021-10-11 09:10:53 -0500

    Se pulio integracion de bd en registro

--- pages/reg.php
+++ pages/reg.php
@@ -16,7 +16,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reg.css">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/reg.css">
     <script src="https://kit.fontawesome.com/fe08a25bb7.js" crossorigin="anonymous"></script>
 
     <!-- Google font -->
@@ -26,9 +28,9 @@
     <title>Registrate</title>
 </head>
 <body>
-    <div class="container">
-        <div class="registro">
-            <form action="thanks.php" method="post">
+    <div class="container pb-20">
+        <div class="registro mt-2">
+            <form method="post" autocomplete="off">
                 <h1><span class="red">Burogu</span> Blog</h1>
                 <div class="formulario">
                     <div>
@@ -57,7 +59,7 @@
                     </div>
                     <div>
                         <p>Selecciona tu avatar: </p>
-                        <select name="Reg_av">
+                        <select name="Reg_av" class="text-black">
                         <?php 
                             while($row = mysqli_fetch_array($res))
                             {
@@ -68,11 +70,16 @@
                         </select>
                     </div>
                 </div>
-                <button>Enviar</button>
+                <div class="buttons">
+                    <!-- <button value="Enviar">Enviar</button> -->
+                    <input type="submit" name="Enviar" value="Enviar" class="text-white p-4 bg-red-500 cursor-pointer hover:bg-red-800">
+                    <button onclick="window.location.href='../index-remodel.html';" class="text-white p-4 bg-red-500 cursor-pointer hover:bg-red-800">Volver</button>
+                </div>
             </form>
-
         </div>
     </div>
-
+    <?php 
+        include 'validate_reg.php';
+    ?>
 </body>
 </html>
